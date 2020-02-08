1148857e8a869115fa3c6676712390287ca2897a
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-02-08 16:31:02 -0500

    a script that when left clicked shoots particles

---
+++ Ability/Assets/Scripts/ParticlePlay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePlay : MonoBehaviour
+{
+    public ParticleSystem particle;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            particle.Play();
+        }
+    }
+}
