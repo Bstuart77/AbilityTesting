e64f50fb73824b1acd4a745fe3316affb8884df5
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-02-22 19:11:21 -0500

    randomely spawns player in game scene throught BoltNetwork

---
+++ Ability/Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    [BoltGlobalBehaviour]
+    public override void SceneLoadLocalDone(string scene)
+    {
+        var spawnPosition = new Vector3(Random.Range(-175, -75), 0, Random.Range(5, 105));
+       BoltNetwork.Instantiate(BoltPrefabs.Cube,spawnPosition,Quaternion.identity);
+    }
+}
