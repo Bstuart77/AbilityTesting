1d9da5aa2052fe8b28f4ff28a7c9d36a3e3d78d5
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-02-22 19:10:41 -0500

    script that allows the hosting and joining of a server

---
+++ Ability/Assets/Scripts/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UdpKit;
+using UnityEngine;
+
+public class Menu : Bolt.GlobalEventListener
+{
+    ///HOST
+    public void startServer()
+    {
+        BoltLauncher.StartServer();
+    }
+    //JOINING GAME
+    public void startClient()
+    {
+        BoltLauncher.StartClient();
+    }
+
+    public override void BoltStartDone()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            string matchName = "Testing";
+            BoltNetwork.SetServerInfo(matchName, null);
+            BoltNetwork.LoadScene("Game");
+        }
+    }
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        foreach(var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if(photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltNetwork.Connect(photonSession);
+            }
+        }
+    }
+}
