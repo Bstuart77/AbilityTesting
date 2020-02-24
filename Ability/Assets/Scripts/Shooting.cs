4c51c0dd350660fe3c86410e0a1fa2a7fe0e0463
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-02-24 01:13:59 -0500

    Added a script that allows the server to pick up on shooting

---
+++ Ability/Assets/Scripts/Shooting.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class Shooting : Bolt.EntityBehaviour<ICustomCubeState>
+{
+    public Rigidbody bulletPrefab;
+    public float bulledSpeed;
+    public GameObject muzzle;
+
+    public override void Attached()
+    {
+        state.OnShoot = Shoot;
+    }
+    public void Shoot()
+    {
+        Rigidbody bulletClone = Instantiate(bulletPrefab, muzzle.transform.position, this.transform.rotation);
+        bulletClone.velocity = transform.TransformDirection(new Vector3(0, 0, bulledSpeed));
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && entity.IsOwner)
+        {
+            state.Shoot();
+        }
+    }
+}
