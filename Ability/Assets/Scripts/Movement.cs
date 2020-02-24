b57a522f50d5e6e877d5cf300aeaa791252fc255
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-02-23 20:30:32 -0500

    added Bolt movement with WASD

--- Ability/Assets/Scripts/Movement.cs
+++ Ability/Assets/Scripts/Movement.cs
@@ -1,31 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    private float moveSpeed = 30f;
-    private Rigidbody rb;
-    private float rotateX = 1.0f;
-    private float rotateY = 1.0f;
-    private float test;
-    private float test2;
 
-    // Start is called before the first frame update
-    void Start()
+public class Movement : Bolt.EntityBehaviour<ICustomCubeState>
+{
+    //void start
+    public override void Attached()
     {
-        rb = GetComponent<Rigidbody>();
+        state.SetTransforms(state.CubeTransform, gameObject.transform);
     }
 
-    // Update is called once per frame
-    void Update()
+    //void update
+    public override void SimulateOwner()
     {
-               transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        var speed = 25f;
+        var movement = Vector3.zero;
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement.x -= 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement.x += 1f;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement.z += 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement.z -= 1f;
+        }
 
-        test += rotateX * Input.GetAxis("Mouse X");
-        test2 -= rotateY * Input.GetAxis("Mouse Y");
-        test2 = Mathf.Clamp(test2, -100f, 100f);
-        transform.eulerAngles = new Vector3(test2, test, 0.0f);
+        if (movement != Vector3.zero)
+        {
+            transform.position = transform.position + movement.normalized * speed * BoltNetwork.FrameDeltaTime;
+        }
     }
 }
