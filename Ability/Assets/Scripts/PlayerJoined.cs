1914792f5138df9ffdb85652a29f3c1add9672f7
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-02-25 18:18:50 -0500

    Script that moves the camera to first person and sends a welcome message when joined

---
+++ Ability/Assets/Scripts/PlayerJoined.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJoined : Bolt.EntityBehaviour<ICustomCubeState>
+{
+    public Camera entityCamera;
+
+    public override void Attached()
+    {
+        var evnt = PlayerJoinedEvent.Create();
+        evnt.Message = "Hello There!";
+        evnt.Send();
+    }
+
+     void Update()
+    {
+        if (entity.IsOwner && entityCamera.gameObject.activeInHierarchy == false)
+        {
+            entityCamera.gameObject.SetActive(true);
+        }
+    }
+}
