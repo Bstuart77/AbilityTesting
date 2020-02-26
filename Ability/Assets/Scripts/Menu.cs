aff7683c0e1890134795320830ee85913a3b7018
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-02-26 17:01:26 -0500

    added a server list, clickable join game under the "Join game"

--- Ability/Assets/Scripts/Menu.cs
+++ Ability/Assets/Scripts/Menu.cs
@@ -3,9 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UdpKit;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Menu : Bolt.GlobalEventListener
 {
+    public Button joinGameButtonPrefab;
+    public GameObject serverListPanel;
+    private List<Button> joinServerButtons = new List<Button>();
+    public float  buttonsSpacing;
     ///HOST
     public void startServer()
     {
@@ -20,22 +24,43 @@ public class Menu : Bolt.GlobalEventListener
     public override void BoltStartDone()
     {
         if (BoltNetwork.IsServer)
-        {
-            string matchName = "Testing";
+        {   
+            int random = UnityEngine.Random.Range(0, 99);
+            string matchName = "Testing" + random;
             BoltNetwork.SetServerInfo(matchName, null);
             BoltNetwork.LoadScene("Game");
         }
     }
     public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
     {
+        clearSessions();
+        int totalSessions = 0;
+        print("Total Sessions" + totalSessions);
         foreach(var session in sessionList)
         {
             UdpSession photonSession = session.Value as UdpSession;
 
-            if(photonSession.Source == UdpSessionSource.Photon)
-            {
-                BoltNetwork.Connect(photonSession);
-            }
+            Button joinGameButtonClone = Instantiate(joinGameButtonPrefab);
+            joinGameButtonClone.transform.parent = serverListPanel.transform;
+            joinGameButtonClone.transform.localPosition = new Vector3(0, buttonsSpacing * joinServerButtons.Count, 0);
+            joinGameButtonClone.gameObject.SetActive(true);
+            joinGameButtonClone.onClick.AddListener(() => joinGame(photonSession));
+
+            joinServerButtons.Add(joinGameButtonClone);
         }
+    }
+    private void joinGame(UdpSession photonsession)
+    {
+        BoltNetwork.Connect(photonsession);
+    }
+    
+    private void clearSessions()
+    {
+        foreach(Button button in joinServerButtons)
+        {
+            Destroy(button.gameObject);
+        }
+        joinServerButtons.Clear();
+
     }
 }
