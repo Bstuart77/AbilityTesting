ba0e5b21928dac5124ea206b97e538c841a0021f
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-02-27 18:54:10 -0500

    fixed bug that cause the player to spawn half undermap

--- MultiPlayer/Assets/Scripts/NetworkCallbacks.cs
+++ MultiPlayer/Assets/Scripts/NetworkCallbacks.cs
@@ -6,7 +6,7 @@ public class NetworkCallbacks : Bolt.GlobalEventListener
     [BoltGlobalBehaviour]
     public override void SceneLoadLocalDone(string scene)
     {
-        var spawnPosition = new Vector3(Random.Range(-25, 25), 0, Random.Range(-2, 10));
+        var spawnPosition = new Vector3(Random.Range(-25, 25), 2.7f, Random.Range(-2, 10));
        BoltNetwork.Instantiate(BoltPrefabs.Cube,spawnPosition,Quaternion.identity);
     }
 }
