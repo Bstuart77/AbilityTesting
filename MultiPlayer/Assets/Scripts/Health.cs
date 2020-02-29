45fb856fb126cb0e077a52b26a491966a65910e8
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-02-29 18:59:36 -0500

    health script that allows the bullet collision and subtracts health

---
+++ MultiPlayer/Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : Bolt.EntityBehaviour<ICustomCubeState>
+{
+    public int localhealth;
+
+    public override void Attached()
+    {
+        state.Health = localhealth;
+        state.AddCallback("Health", HealthCallBack);
+    }
+
+    private void HealthCallBack()
+    {
+        localhealth = state.Health;
+        
+        if (localhealth <= 0)
+        {
+            BoltNetwork.Destroy(gameObject);
+        }
+    }
+   
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.name == "Bullet(Clone)")
+        {
+            localhealth -= 1;
+            print("HIT");
+
+            BoltNetwork.Destroy(collision.gameObject);
+            
+            if (localhealth <= 0)
+            {
+                BoltNetwork.Destroy(gameObject);    
+                print("KILLED");
+            }
+        }
+    }
+}
