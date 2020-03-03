7b5a308b50d64abada54de7996a9a63275299a87
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-03-03 15:10:58 -0500

    updated script to allow freelook

--- MultiPlayer/Assets/Scripts/Movement.cs
+++ MultiPlayer/Assets/Scripts/Movement.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Movement : Bolt.EntityBehaviour<ICustomCubeState>
 {
-    //void start
+    private float sens = 100f;
+    public Transform playerbody;
+    private float xRotate = 0;
+    private float yRotate = 0;
+
     public override void Attached()
     {
         state.SetTransforms(state.CubeTransform, gameObject.transform);
     }
 
-    //void update
+   /* private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }*/
+
     public override void SimulateOwner()
     {
         var speed = 25f;
         var movement = Vector3.zero;
+      
+       
+
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -37,5 +48,18 @@ public class Movement : Bolt.EntityBehaviour<ICustomCubeState>
         {
             transform.position = transform.position + movement.normalized * speed * BoltNetwork.FrameDeltaTime;
         }
+
+        float mouseX = Input.GetAxis("Mouse X") * sens * BoltNetwork.FrameDeltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sens * BoltNetwork.FrameDeltaTime;
+
+        xRotate -= mouseY;
+        yRotate += mouseX;
+
+        xRotate = Mathf.Clamp(xRotate, -90, 90);
+
+       
+        transform.localRotation = Quaternion.Euler(xRotate, yRotate, 0f);
+        playerbody.Rotate(Vector3.up * mouseX);
+        playerbody.Rotate(Vector3.left * mouseY);
     }
 }
