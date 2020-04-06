20656e48ece420bebd2b3bf37425d672635bd2ef
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-04-05 21:23:24 -0400

    moved into Folders

---
+++ MultiPlayer/Assets/Scripts/CollectGuns/CollectPistol.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectPistol : Bolt.GlobalEventListener
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.name == "Cube(Clone)")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ MultiPlayer/Assets/Scripts/SpawnGuns/SpawnPistol.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPistol : Bolt.GlobalEventListener
+{
+    public GameObject pistol;
+    [BoltGlobalBehaviour]
+    public override void SceneLoadLocalDone(string scene)
+    {
+        var spawnPosition = new Vector3(0, .58f, .075f);
+        BoltNetwork.Instantiate(pistol, spawnPosition, Quaternion.identity);
+    }
+}
