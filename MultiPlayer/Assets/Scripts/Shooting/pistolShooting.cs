f9425ca6ff3d1c4a6eacb3590c2c599d22a9f7ab
Brian <bstuart777@gmail.com>
2020-06-04 23:00:32 -0400

    deleted reference to a deleted script

--- MultiPlayer/Assets/Scripts/Shooting/pistolShooting.cs
+++ MultiPlayer/Assets/Scripts/Shooting/pistolShooting.cs
@@ -15,7 +15,6 @@ public class pistolShooting : Bolt.EntityBehaviour<ICustomCubeState>
     {
         Rigidbody bulletClone = Instantiate(bulletPrefab, muzzle.transform.position, this.transform.rotation);
         bulletClone.velocity = transform.TransformDirection(new Vector3(0, 0, bulledSpeed));
-        bulletClone.GetComponent<bullet>().shootingScript = this;
     }
     private void Update()
     {
