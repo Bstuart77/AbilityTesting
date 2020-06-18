405f22ca4378313620bb2d5e4c2f02a977d081a8
Brian <bstuart777@gmail.com>
2020-06-18 04:33:16 -0400

    script that keeps track of global scores

---
+++ MultiPlayer/Assets/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : Bolt.EntityBehaviour<ICustomCubeState>
+{
+    public int score;
+
+    public void changeScore()
+    {
+        if (entity.IsOwner)
+        {
+            score++;
+
+            var scoreEvent = ScoreEvent.Create();
+            scoreEvent.Message = PlayerPrefs.GetString("username") + " scored " + score;
+            scoreEvent.Send();
+        }
+    }
+}
