4238b932770f7b6ab2bbd76b891f50aabea75389
Brian <bstuart777@gmail.com>
2020-06-18 04:28:25 -0400

    displays on console the scoreevent message

---
+++ MultiPlayer/Assets/GameCanvas.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCanvas : Bolt.GlobalEventListener
+{
+    public override void OnEvent(ScoreEvent evnt)
+    {
+        print(evnt.Message);
+    }
+}
