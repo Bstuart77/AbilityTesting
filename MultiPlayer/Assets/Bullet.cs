1f84527ad09dbad0158518785f388f4e9b562050
Brian <bstuart777@gmail.com>
2020-06-18 04:28:06 -0400

    Bullet script that keeps track what bullet hits what

---
+++ MultiPlayer/Assets/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [HideInInspector]
+    public PistolShooting shootingScript;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacles")
+        {
+            shootingScript.scoreScript.changeScore();
+            Destroy(gameObject);
+        }
+    }
+}
