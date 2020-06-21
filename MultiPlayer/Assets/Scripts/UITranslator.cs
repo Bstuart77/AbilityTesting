068c1b0b979e05acc362020d8fcaa8b4e5d46ec9
Brian <bstuart777@gmail.com>
2020-06-20 23:54:43 -0400

    script to hold the formatting of the scene canvas

---
+++ MultiPlayer/Assets/Scripts/UITranslator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public static class UITranslator
+{
+    public static void TranslateTextToUIPostion(TextMeshProUGUI text, RectTransform desiredSize, GameObject parent)
+    {
+        text.transform.parent = parent.transform;
+        text.GetComponent<RectTransform>().sizeDelta = desiredSize.sizeDelta;
+        text.GetComponent<RectTransform>().localScale = desiredSize.localScale;
+        text.GetComponent<RectTransform>().position = desiredSize.position;
+
+        text.gameObject.SetActive(true);
+    }
+}
