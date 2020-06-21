9bb709dfb147a7e6a783cd0b8cbd3b2aad8b98e7
Brian <bstuart777@gmail.com>
2020-06-20 23:54:26 -0400

    added score UI

--- MultiPlayer/Assets/GameCanvas.cs
+++ MultiPlayer/Assets/GameCanvas.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameCanvas : Bolt.GlobalEventListener
 {
+    public GameObject canvas;
+    public TextMeshProUGUI scorePopupTemplate;
     public override void OnEvent(ScoreEvent evnt)
     {
-        print(evnt.Message);
+        //print(evnt.Message);
+        TextMeshProUGUI scorePopupClone = Instantiate(scorePopupTemplate);
+        scorePopupClone.text = evnt.Message;
+        scorePopupClone.gameObject.SetActive(true);
+        UITranslator.TranslateTextToUIPostion(scorePopupClone, scorePopupTemplate.GetComponent<RectTransform>(), canvas);
+        Destroy(scorePopupClone.gameObject, 4f);
     }
 }
