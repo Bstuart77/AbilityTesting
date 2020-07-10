a7459778c73ed99aa893303b3e8c845258ac6afa
Brian <bstuart777@gmail.com>
2020-07-10 01:05:34 -0400

    added UI elemes to increment and PistolShooting script

--- MultiPlayer/Assets/Scripts/ServerSide/Health.cs
+++ MultiPlayer/Assets/Scripts/ServerSide/Health.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : Bolt.EntityBehaviour<ICustomCubeState>
 {
     public int localhealth;
     private Rigidbody rb;
+    public PistolShooting shootingScript;
+    public Text killScore;
+    public static double killAmt = 0;
+
     public override void Attached()
     {
         state.Health = localhealth;
@@ -14,12 +19,17 @@ public class Health : Bolt.EntityBehaviour<ICustomCubeState>
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        localhealth = 5;
     }
     private void HealthCallBack()
     {
         localhealth = state.Health;
     }
-   
+
+    private void Update()
+    {
+        killScore.text = killAmt.ToString();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -29,12 +39,14 @@ public class Health : Bolt.EntityBehaviour<ICustomCubeState>
             rb.velocity = new Vector3(0, 0, 0);
 
             BoltNetwork.Destroy(collision.gameObject);
-            
+
             if (localhealth <= 0)
             {
                 var spawnPosition = new Vector3(Random.Range(39, 122), 38f, Random.Range(34, -45));
                 transform.position = spawnPosition;
                 localhealth = 5;
+
+
             }
         }
     }
