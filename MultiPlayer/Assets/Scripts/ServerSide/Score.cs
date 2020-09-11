47d57d30fa9d8848491c793a0778c6e39b11b0c8
Brian <bstuart777@gmail.com>
2020-09-11 03:29:53 -0400

    uneeded

--- MultiPlayer/Assets/Scripts/ServerSide/Score.cs
+++ MultiPlayer/Assets/Scripts/ServerSide/Score.cs
@@ -6,11 +6,4 @@ public class Score : Bolt.EntityBehaviour<ICustomCubeState>
 {
     public static int killAmt = 0;
     public static int deathAmt = 0;
-    public void changeScore()
-    {
-        if (entity.IsOwner && Health.localhealth <=0)
-        {
-
-        }
-    }
 }
