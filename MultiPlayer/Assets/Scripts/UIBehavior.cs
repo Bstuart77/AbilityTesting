347d98df745b8b95bcff5b77582d7216edb8c6d0
Brian <bstuart777@gmail.com>
2020-09-11 02:17:10 -0400

    added UI element to track health

---
+++ MultiPlayer/Assets/Scripts/UIBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIBehavior : MonoBehaviour
+{
+    public Text healthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = Health.localhealth.ToString("0");
+    }
+}
