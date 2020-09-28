ed20046908dae01c24c290d724906ce2df9466a2
Brian <bstuart777@gmail.com>
2020-09-28 01:46:07 -0400

    added a script to keep track of kills,deaths, health

--- MultiPlayer/Assets/Scripts/ServerSide/Score.cs
+++ MultiPlayer/Assets/Scripts/ServerSide/Score.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : Bolt.EntityBehaviour<ICustomCubeState>
 {
     public static int killAmt = 0;
     public static int deathAmt = 0;
+    public Text killScore;
+    public Text deathScore;
+    public Text healthScore;
+
+
+    private void Update()
+    {
+        killScore.text = killAmt.ToString();
+        deathScore.text = deathAmt.ToString();
+        healthScore.text = Health.localhealth.ToString();
+    }
 }
