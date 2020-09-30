697d658f4845a62c491664d1c88f0d139f5e6109
Brian <bstuart777@gmail.com>
2020-09-29 20:11:49 -0400

    formatted code to show scoreboard when joining

--- MultiPlayer/Assets/Scripts/ServerSide/Score.cs
+++ MultiPlayer/Assets/Scripts/ServerSide/Score.cs
@@ -11,9 +11,16 @@ public class Score : Bolt.EntityBehaviour<ICustomCubeState>
     public Text deathScore;
     public Text healthScore;
 
+    public GameObject scoreboardPrefab;
+    public float UISpacing;
+    private List<GameObject> updateUI = new List<GameObject>();
 
     private void Update()
     {
+        BoltNetwork.Instantiate(scoreboardPrefab);
+        scoreboardPrefab.transform.localPosition = new Vector3(0, UISpacing * updateUI.Count, 0);
+        scoreboardPrefab.gameObject.SetActive(true);
+
         killScore.text = killAmt.ToString();
         deathScore.text = deathAmt.ToString();
         healthScore.text = Health.localhealth.ToString();
