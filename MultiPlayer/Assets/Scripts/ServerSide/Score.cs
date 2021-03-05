b51d6df2bd0499e077665b9c24f9257e98ee6841
Brian <bstuart777@gmail.com>
2021-03-04 19:08:35 -0500

    synced up input field to UI name

--- MultiPlayer/Assets/Scripts/ServerSide/Score.cs
+++ MultiPlayer/Assets/Scripts/ServerSide/Score.cs
@@ -9,10 +9,12 @@ public class Score : Bolt.EntityBehaviour<ICustomCubeState>
     public static int deathAmt = 0;
     public Text killScore;
     public Text deathScore;
+    public Text name;
 
     private void Update()
     {
         killScore.text = killAmt.ToString();
         deathScore.text = deathAmt.ToString();
+        name.text = PlayerPrefs.GetString("username").ToString();
     }
 }
