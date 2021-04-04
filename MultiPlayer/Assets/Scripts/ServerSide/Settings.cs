4b37ac735b88ae99f6849912b4d1b224652762ef
Brian <bstuart777@gmail.com>
2021-04-04 00:29:49 -0400

    set up variables to hold the users settings

---
+++ MultiPlayer/Assets/Scripts/ServerSide/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+
+   [HideInInspector] public static float sens = 500f;
+    public AudioSource music;
+    public AudioSource soundEffects;
+    public GameObject pauseMenu;
+    public GameObject audioUI;
+    public GameObject sensUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
