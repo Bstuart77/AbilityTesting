87c2399dc67880b2b2aacdfb512b148c169b7c1b
Brian <bstuart777@gmail.com>
2021-04-03 21:12:27 -0400

    script that toggles the scoreboard on/off

---
+++ MultiPlayer/Assets/Scripts/ServerSide/ShowScoreboard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowScoreboard : MonoBehaviour
+{
+
+    public GameObject scoreboard;
+    public bool isShowing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         scoreboard.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (isShowing)
+            {
+                scoreboard.SetActive(false);
+                isShowing = false;
+            }
+            else
+            {
+                scoreboard.SetActive(true);
+                isShowing = true;
+            }
+        }
+    }
+}
